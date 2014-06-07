bba083ee8db5b335fc96fa2c20e0fa0b78c4eaa2
Arne Kiesewetter <banane1996@gmx.net>
2014-06-07 14:18:09 +1200

    Added all implementation

---
+++ SharpPlugins/PluginBase.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SharpPlugins
+{
+    public class PluginBase
+    {
+        public static string GetAuthor(Type type)
+        {
+            var attribute = getAttribute(type);
+            return attribute == null ? string.Empty : attribute.Author;
+        }
+
+        public static string GetDescription(Type type)
+        {
+            var attribute = getAttribute(type);
+            return attribute == null ? string.Empty : attribute.Description;
+        }
+
+        public static string GetName(Type type)
+        {
+            var attribute = getAttribute(type);
+            return attribute == null ? string.Empty : attribute.Name;
+        }
+
+        public static string GetVersion(Type type)
+        {
+            var attribute = getAttribute(type);
+            return attribute == null ? string.Empty : attribute.Version;
+        }
+
+        public static bool IsRegisteredPlugin(Type type)
+        {
+            return getAttribute(type) != null;
+        }
+
+        private static RegisterPluginAttribute getAttribute(Type type)
+        {
+            return (RegisterPluginAttribute)type.GetCustomAttributes(typeof(RegisterPluginAttribute), false).FirstOrDefault();
+        }
+
+        [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+        protected sealed class RegisterPluginAttribute : Attribute
+        {
+            public string Author { get; set; }
+
+            public string Description { get; set; }
+
+            public string Name { get; set; }
+
+            public string Version { get; set; }
+        }
+    }
+}
---
+++ SharpPlugins/PluginLoader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace SharpPlugins
+{
+    public static class PluginLoader
+    {
+        public static IEnumerable<TPlugin> InstanciatePlugins<TPlugin>(IEnumerable<Type> types) where TPlugin : PluginBase
+        {
+            return InstanciatePlugins<TPlugin>(types, new object[0]);
+        }
+
+        public static IEnumerable<TPlugin> InstanciatePlugins<TPlugin>(IEnumerable<Type> types, params object[] args) where TPlugin : PluginBase
+        {
+            return from type in types
+                   where IsInstanciablePlugin(type)
+                   select (TPlugin)Activator.CreateInstance(type, args);
+        }
+
+        public static bool IsInstanciablePlugin(Type type)
+        {
+            return PluginBase.IsRegisteredPlugin(type) && !type.IsAbstract;
+        }
+
+        public static IEnumerable<Type> LoadPluginsFromFiles<TPlugin>(IEnumerable<string> pluginFiles) where TPlugin : PluginBase
+        {
+            return from pluginFile in pluginFiles
+                   let assembly = Assembly.LoadFrom(pluginFile)
+                   from type in assembly.GetExportedTypes()
+                   where IsInstanciablePlugin(type)
+                   select type;
+        }
+
+        public static IEnumerable<Type> LoadPluginsFromFolders<TPlugin>(IEnumerable<string> pluginFolders) where TPlugin : PluginBase, new()
+        {
+            return from pluginFolder in pluginFolders
+                   let files = Directory.EnumerateFiles(pluginFolder, "*.dll", SearchOption.AllDirectories)
+                   let types = LoadPluginsFromFiles<TPlugin>(files)
+                   from type in types
+                   select type;
+        }
+    }
+}
