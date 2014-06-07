c3a1eb350a70497bfea0c86e31df60910edacd6d
Arne Kiesewetter <banane1996@gmx.net>
2014-06-07 14:45:55 +1200

    Added documentation and a few little changes.

--- SharpPlugins/PluginBase.cs
+++ SharpPlugins/PluginBase.cs
@@ -4,52 +4,116 @@ using System.Linq;
 
 namespace SharpPlugins
 {
-    public class PluginBase
+    /// <summary>
+    /// The abstract base class for plugins.
+    /// </summary>
+    public abstract class PluginBase
     {
+        /// <summary>
+        /// Gets the plugin's Author, or empty string if it fails.
+        /// </summary>
+        /// <param name="type">The plugin's Type.</param>
+        /// <returns>The plugin's Author, or empty string if it fails.</returns>
         public static string GetAuthor(Type type)
         {
             var attribute = getAttribute(type);
-            return attribute == null ? string.Empty : attribute.Author;
+            return attribute == null ? string.Empty : attribute.Author != null ? string.Empty : attribute.Author;
         }
 
+        /// <summary>
+        /// Gets the plugin's Description, or empty string if it fails.
+        /// </summary>
+        /// <param name="type">The plugin's Type.</param>
+        /// <returns>The plugin's Description, or empty string if it fails.</returns>
         public static string GetDescription(Type type)
         {
             var attribute = getAttribute(type);
-            return attribute == null ? string.Empty : attribute.Description;
+            return attribute == null ? string.Empty : attribute.Description != null ? string.Empty : attribute.Description;
         }
 
+        /// <summary>
+        /// Gets the plugin's Name, or empty string if it fails.
+        /// </summary>
+        /// <param name="type">The plugin's Type.</param>
+        /// <returns>The plugin's Name, or empty string if it fails.</returns>
         public static string GetName(Type type)
         {
             var attribute = getAttribute(type);
-            return attribute == null ? string.Empty : attribute.Name;
+            return attribute == null ? string.Empty : attribute.Name != null ? string.Empty : attribute.Name;
         }
 
+        /// <summary>
+        /// Gets the plugin's Version, or empty string if it fails.
+        /// </summary>
+        /// <param name="type">The plugin's Type.</param>
+        /// <returns>The plugin's Version, or empty string if it fails.</returns>
         public static string GetVersion(Type type)
         {
             var attribute = getAttribute(type);
-            return attribute == null ? string.Empty : attribute.Version;
+            return attribute == null ? string.Empty : attribute.Version != null ? string.Empty : attribute.Version;
         }
 
+        /// <summary>
+        /// Gets whether the Type is a registered plugin or not.
+        /// </summary>
+        /// <param name="type">The plugin's Type.</param>
+        /// <returns>Whether the Type is a registered plugin or not.</returns>
         public static bool IsRegisteredPlugin(Type type)
         {
             return getAttribute(type) != null;
         }
 
+        /// <summary>
+        /// Gets the plugin's RegisteredPluginAttribute, or null if it fails.
+        /// </summary>
+        /// <param name="type">The plugin's Type.</param>
+        /// <returns>The plugin's RegisteredPluginAttribute, or null if it fails.</returns>
         private static RegisterPluginAttribute getAttribute(Type type)
         {
             return (RegisterPluginAttribute)type.GetCustomAttributes(typeof(RegisterPluginAttribute), false).FirstOrDefault();
         }
 
+        /// <summary>
+        /// Marks a PluginBase derived class as to be loaded and contains some optional information about the plugin.
+        /// </summary>
         [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
         protected sealed class RegisterPluginAttribute : Attribute
         {
-            public string Author { get; set; }
+            /// <summary>
+            /// Gets the plugin's Author.
+            /// </summary>
+            public string Author { get; private set; }
 
-            public string Description { get; set; }
+            /// <summary>
+            /// Gets the plugin's Description.
+            /// </summary>
+            public string Description { get; private set; }
 
-            public string Name { get; set; }
+            /// <summary>
+            /// Gets the plugin's Name.
+            /// </summary>
+            public string Name { get; private set; }
 
-            public string Version { get; set; }
+            /// <summary>
+            /// Gets the plugin's Version.
+            /// </summary>
+            public string Version { get; private set; }
+
+            /// <summary>
+            /// Creates a new instance of the <see cref="SharpPlugins.PluginBase.RegisterPluginAttribute"/> class
+            /// with the given optional information about the plugin to mark it as to be loaded.
+            /// </summary>
+            /// <param name="author">The plugin's Author.</param>
+            /// <param name="description">The plugin's Description.</param>
+            /// <param name="name">The plugin's Name.</param>
+            /// <param name="version">The plugin's Version.</param>
+            public RegisterPluginAttribute(string author = "", string description = "", string name = "", string version = "")
+            {
+                Author = author;
+                Description = description;
+                Name = name;
+                Version = version;
+            }
         }
     }
 }
--- SharpPlugins/PluginLoader.cs
+++ SharpPlugins/PluginLoader.cs
@@ -6,13 +6,29 @@ using System.Reflection;
 
 namespace SharpPlugins
 {
+    /// <summary>
+    /// Contains static methods for loading plugin types from .dll files.
+    /// </summary>
     public static class PluginLoader
     {
+        /// <summary>
+        /// Creates instances of all the given types that are instanciable plugins.
+        /// </summary>
+        /// <typeparam name="TPlugin">The PluginBase derived plugin type.</typeparam>
+        /// <param name="types">The types to create instances from.</param>
+        /// <returns>Instances of all the types that are instanciable plugins.</returns>
         public static IEnumerable<TPlugin> InstanciatePlugins<TPlugin>(IEnumerable<Type> types) where TPlugin : PluginBase
         {
             return InstanciatePlugins<TPlugin>(types, new object[0]);
         }
 
+        /// <summary>
+        /// Creates instances of all the given Types that are instanciable plugins using the given arguments for the constructor.
+        /// </summary>
+        /// <typeparam name="TPlugin">The PluginBase derived plugin type.</typeparam>
+        /// <param name="types">The Types to create instances from.</param>
+        /// <param name="args">The arguments for the constructor.</param>
+        /// <returns>Instances of all the types that are instanciable plugins.</returns>
         public static IEnumerable<TPlugin> InstanciatePlugins<TPlugin>(IEnumerable<Type> types, params object[] args) where TPlugin : PluginBase
         {
             return from type in types
@@ -20,23 +36,43 @@ namespace SharpPlugins
                    select (TPlugin)Activator.CreateInstance(type, args);
         }
 
+        /// <summary>
+        /// Determines whether the given Type is an instanciable plugin or not.
+        /// </summary>
+        /// <param name="type">The Type to check.</param>
+        /// <returns>Whether the given Type is an instanciable plugin or not.</returns>
         public static bool IsInstanciablePlugin(Type type)
         {
             return PluginBase.IsRegisteredPlugin(type) && !type.IsAbstract;
         }
 
+        /// <summary>
+        /// Takes file paths and checks if each exists, and loads .dll files to find the instanciable plugin types deriving from TPlugin.
+        /// </summary>
+        /// <typeparam name="TPlugin">The Type that the plugins have to be derived from.</typeparam>
+        /// <param name="pluginFiles">The paths to the .dll files to check.</param>
+        /// <returns>The Types of the found instanciable plugins.</returns>
         public static IEnumerable<Type> LoadPluginsFromFiles<TPlugin>(IEnumerable<string> pluginFiles) where TPlugin : PluginBase
         {
             return from pluginFile in pluginFiles
+                   where File.Exists(pluginFile) && Path.GetExtension(pluginFile).Equals("dll", StringComparison.InvariantCultureIgnoreCase)
                    let assembly = Assembly.LoadFrom(pluginFile)
                    from type in assembly.GetExportedTypes()
-                   where IsInstanciablePlugin(type)
+                   where typeof(TPlugin).IsAssignableFrom(type) && IsInstanciablePlugin(type)
                    select type;
         }
 
+        /// <summary>
+        /// Takes directory paths and checks if each exists, and loads .dll files, including from subfolders,
+        /// to find the instanciable plugin types deriving from TPlugin.
+        /// </summary>
+        /// <typeparam name="TPlugin">The Type that the plugins have to be derived from.</typeparam>
+        /// <param name="pluginFolders">The paths to the folders to check.</param>
+        /// <returns>The Types of the found instanciable plugins.</returns>
         public static IEnumerable<Type> LoadPluginsFromFolders<TPlugin>(IEnumerable<string> pluginFolders) where TPlugin : PluginBase, new()
         {
             return from pluginFolder in pluginFolders
+                   where Directory.Exists(pluginFolder)
                    let files = Directory.EnumerateFiles(pluginFolder, "*.dll", SearchOption.AllDirectories)
                    let types = LoadPluginsFromFiles<TPlugin>(files)
                    from type in types
